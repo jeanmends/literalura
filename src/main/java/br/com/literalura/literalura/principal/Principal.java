ad34479230f062ab95621c01137f66d0dedc896a
Jean Mendes <jeanmenreis@hotmail.com>
2024-07-15 20:48:59 -0300

    final

--- src/main/java/br/com/literalura/literalura/principal/Principal.java
+++ src/main/java/br/com/literalura/literalura/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.literalura.literalura.principal;
 
+import br.com.literalura.literalura.model.Autor;
 import br.com.literalura.literalura.model.DadosLivro;
 import br.com.literalura.literalura.model.Livro;
 import br.com.literalura.literalura.repository.LivroRepository;
@@ -7,8 +8,12 @@ import br.com.literalura.literalura.service.ConsumoApi;
 import br.com.literalura.literalura.service.ConverteDados;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -33,6 +38,10 @@ public class Principal {
                     1 - Buscar livros pelo título
                     2 - Listar livros registrados
                     3 - Listar Autores registrados
+                    4 - Listar autores vivos em um determinado ano
+                    5 - Listar livros em um determinado idioma
+                                        
+                    0 - Sair
                     """;
 
             System.out.println(menu);
@@ -47,21 +56,32 @@ public class Principal {
                     mostrarLivros();
                     break;
                 case 3:
-                    mostrarAtuores();
+                    mostrarAutores();
+                    break;
+                case 4:
+                    mostrarAutoresVivos();
+                    break;
+                case 5:
+                    pesquiarPorIdiomas();
                     break;
 
-
+                case 0:
+                    System.out.println("Sistema encerrado!");
+                    break;
+                default:
+                    System.out.println("Menu inválido");
+                    break;
             }
         }
     }
 
 
-
-    private void salvarLivros(List<Livro> livros){
+    private void salvarLivros(List<Livro> livros) {
 
         livros.forEach(livroRepository::save);
     }
-    private void getDadosLivro(){
+
+    private void getDadosLivro() {
         System.out.println("Digite o nome do livro: ");
         var nomeLivro = leitura.nextLine();
         var enderecoApi = API_KEY + nomeLivro.replace(" ", "+");
@@ -76,20 +96,27 @@ public class Principal {
         }
         JsonNode resultsNode = rootNode.path("results");
         String convertedToString = String.valueOf(resultsNode);
-       // DadosLivro dados = conversor.obterDados(convertedToString.substring(1, convertedToString.length() -1), DadosLivro.class);
+        // DadosLivro dados = conversor.obterDados(convertedToString.substring(1, convertedToString.length() -1), DadosLivro.class);
 
         try {
             List<DadosLivro> livros = conversor.getObjectMapper()
                     .readerForListOf(DadosLivro.class)
                     .readValue(resultsNode);
 
+            if (livros.isEmpty()) {
+                System.out.println("Nada encontrado");
+            }
+
             List<Livro> novosLivros = livros.stream().map(Livro::new).collect(Collectors.toList());
-            //System.out.println(novosLivros);
-            salvarLivros(novosLivros);
+            novosLivros.stream().forEach(livroRepository::save);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (DataIntegrityViolationException | IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+            ;
         }
+
     }
 
     private void mostrarLivros() {
@@ -102,21 +129,71 @@ public class Principal {
         }
     }
 
-    private void mostrarAtuores() {
-       List<Livro> livros = livroRepository.findAll();
+    private void mostrarAutores() {
+        List<Livro> livros = livroRepository.findAll();
 
         if (livros.isEmpty()) {
-            System.out.println("Nenhum autor registrado.");
+            System.out.println("Nenhum autor cadastrado.");
         } else {
             livros.stream()
                     .map(Livro::getAutor)
                     .distinct()
-                    .forEach(autor -> System.out.println(autor.getAutor()));
+                    .forEach(a -> System.out.println(a.getAutor()));
         }
 
 
+    }
+
+    private void mostrarAutoresVivos() {
+        System.out.println("Digite ano: ");
+        int ano = leitura.nextInt();
+        Year year = Year.of(ano);
+
+        List<Autor> autores = livroRepository.findAutoresVivos(year);
+        if (autores.isEmpty()) {
+            System.out.println("Não foi encontrado no ano informado!");
+        } else {
+            autores.forEach(a -> System.out.println("Nome: " + a.getAutor() +
+                    "\nAno nascimento: " + a.getAnoNascimento() +
+                    "\nAno falescimento: " + a.getAnoFalecimento() +
+                    "\n\n-----------------------------------------------------"));
+        }
 
 
     }
+
+    private void mostrarIdiomas() {
+        List<Livro> livros = livroRepository.findAll();
+
+        if (livros.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado.");
+        } else {
+            System.out.println("Menu idioma: \n");
+            livros.stream().map(Livro::getIdioma)
+                    .distinct().forEach(System.out::println);
+        }
+    }
+
+    private void pesquiarPorIdiomas() {
+        List<Livro> livros = livroRepository.findAll();
+
+        if (livros.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado.");
+        } else {
+            mostrarIdiomas();
+            System.out.println(" ");
+            System.out.println("Digite o idioma: ");
+            var idioma = leitura.nextLine();
+
+            List<Livro> encontrado = livros.stream().filter(a -> a.getIdioma().equalsIgnoreCase(idioma)).collect(Collectors.toList());
+
+            if (encontrado.isEmpty()) {
+                System.out.println("Idioma não encontrado! ");
+            } else {
+                encontrado.forEach(a -> System.out.println(a.getTitulo()));
+            }
+        }
+    }
+
 
 }
--- src/main/java/br/com/literalura/literalura/repository/LivroRepository.java
+++ src/main/java/br/com/literalura/literalura/repository/LivroRepository.java
@@ -4,9 +4,15 @@ import br.com.literalura.literalura.model.Autor;
 import br.com.literalura.literalura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.Year;
+import java.util.Date;
 import java.util.List;
 
 public interface LivroRepository  extends JpaRepository <Livro, Long> {
+    @Query("SELECT a FROM Autor a WHERE a.anoNascimento <= :ano AND (a.anoFalecimento IS NULL OR a.anoFalecimento >= :ano)")
+    List<Autor> findAutoresVivos(@Param("ano") Year year);
+
 
 }
